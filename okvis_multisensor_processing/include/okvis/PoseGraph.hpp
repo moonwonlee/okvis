d6d285cffa847290715b802f0f6d59ce5eb2a8c4
Joseph Menke <joemenke@berkeley.edu>
2018-02-06 20:49:26 -0800

    minor reorganization

--- okvis_multisensor_processing/include/okvis/PoseGraph.hpp
+++ okvis_multisensor_processing/include/okvis/PoseGraph.hpp
@@ -198,7 +198,242 @@ class PoseGraph {
   };
 
   void processKeyFrame(KeyFrameData::Ptr kf){
+ //All of the below should be moved to the pose graph.hpp file
+    //compute most optimized location of the current keyframe
+    currentKeyframeT_WSo = currentKeyframeT_WSo*kf->T_SoSn;
 
+    //This is only for debugging purposes
+    Pose3dNode node;
+    node.q = kf->T_WS.q();
+    node.p = kf->T_WS.r();
+    originalNodes_[poses_.size()]=node;
+    
+    //Update the new keyframe pose using thew best estimate from the optimized pose graph
+    //kf->T_WS = currentKeyframeT_WSo;
+    node.q = currentKeyframeT_WSo.q();
+    node.p = currentKeyframeT_WSo.r();
+    //Add this node to the pose graph
+    nodes_[poses_.size()]=node;
+
+    //As long as this is not the first pose
+    if(poses_.size()>0){
+      //add constraint to pose graph between this pose and previous pose
+      Pose3dNode newDiffConstraintNode;
+      newDiffConstraintNode.q = kf->T_SoSn.q();
+      newDiffConstraintNode.p = kf->T_SoSn.r();
+
+      Constraint3dNode newDiffConstraint;
+      newDiffConstraint.t_be = newDiffConstraintNode;
+      newDiffConstraint.id_begin = poses_.size()-1;
+      newDiffConstraint.id_end = poses_.size();
+      newDiffConstraint.information = Eigen::Matrix<double,6,6>::Identity();
+      constraints_.push_back(newDiffConstraint);
+
+      //I also include a "gravity constraint" 
+      //This represents the fact that the tilt and roll estimates are bounded due to 
+      //the presence of an accelerometer
+      GravityNode newGravityConstraint;
+      newGravityConstraint.g = kf->T_WS.q()*Eigen::Vector3d(0,0,1);
+      newGravityConstraint.id = poses_.size();
+      newGravityConstraint.information = Eigen::Matrix3d::Identity();
+      gravity_.push_back(newGravityConstraint);
+
+    }
+
+    //These are for debugging purposes
+    OutputPoses("poses.txt");
+    OutputPoses("orig_poses.txt", originalNodes_);
+
+
+    //Get feature points  
+    std::vector<cv::KeyPoint> points;
+    points.reserve(kf->observations.size());
+    for (size_t k = 0; k < kf->observations.size(); ++k) {
+      cv::KeyPoint kp;
+      kf->keyFrames->getCvKeypoint(0,kf->observations[k].keypointIdx,kp);
+
+      if(!okvis::in_image(kp.pt,kf->keyFrames->image(0))){
+        std::cout << "Point Not in Image\n";
+      }
+      points.emplace_back(kp);
+    }
+
+    //detect orb descriptors
+    //later will just use brisk descriptors, but need to create proper DBoW2 templates first
+    cv::Ptr<cv::ORB> orb = cv::ORB::create();
+    orb->compute(kf->keyFrames->image(0),points,kf->descriptors);
+
+    //if we can not compute orb descriptors for all points, do not use keyframe
+    //this will not be an issue once we switch to brisk descriptors
+    if(points.size()!=kf->observations.size())
+      return;
+
+    //convert to DBoW descriptor format
+    std::vector<cv::Mat> bowDesc;
+    bowDesc.reserve(kf->descriptors.rows);
+    for(int i=0; i<kf->descriptors.rows; i++){
+      bowDesc.emplace_back(kf->descriptors.row(i));
+    } 
+
+    //get BoW vector for keyframe 
+    vocab_->transform(bowDesc, kf->bowVec);
+    if(kf->bowVec.size()==0)
+      return;
+    if(posesSinceLastLoop_<20){
+      //20 is the min number of keyframes before we want to consider a loop closure
+      //this prevents us from trying to close the loop wuth very nearby frames
+      lastEntry_ = kf->id = db_->add(kf->bowVec);
+      poses_.push_back(kf);
+      posesSinceLastLoop_++;
+      return;
+    }
+
+    DBoW2::QueryResults qret;
+    //get best result from database. Don't consider the 20 most recent images
+    //should experiment with getting top 3 or top 5 results from database
+    db_->query(kf->bowVec,qret,1,lastEntry_-20);
+    //compute similarity score to previous image
+    //this is necessary as the score will depend on the number and type of features in the image
+    //see DBoW2 paper for more details
+    float baseScore = vocab_->score(kf->bowVec,poses_[lastEntry_]->bowVec);
+    if(qret.size()>0 && baseScore>0.1 && qret[0].Score/baseScore>0.75){ 
+      //.01 is min similarity to previous frame,
+      //.75 is required similarity of matched frame relative to previous frame
+      //that is, the matched frame must be at least 75% as similar to the current
+      //frame as the current frame is to the previous frame
+
+      //if the matched frame is similar enough, attempt to compute SE3 transform
+      PoseGraph::KeyFrameData::Ptr matchedFrame = poses_[qret[0].Id];
+
+      //get keypoints from frame
+      std::vector<cv::KeyPoint> matchedPoints;
+      matchedPoints.reserve(matchedFrame->observations.size());
+      for (size_t k = 0; k < matchedFrame->observations.size(); ++k) {
+        cv::KeyPoint kp;
+        matchedFrame->keyFrames->getCvKeypoint(0,matchedFrame->observations[k].keypointIdx,kp);
+
+        if(!okvis::in_image(kp.pt,matchedFrame->keyFrames->image(0))){
+          std::cout << "Point Not in Image\n";
+        }
+        matchedPoints.emplace_back(kp);
+      }
+
+      //match them to the current frame using optical flow 
+      std::vector<cv::Point2f> matchedFeatures, newFeatures, newFeaturesFinal;
+      okvis::keypoints_to_features(matchedPoints, matchedFeatures);
+
+      std::vector<unsigned char> valid;
+      std::vector<float> err;
+
+      calcOpticalFlowPyrLK(
+        matchedFrame->keyFrames->image(0), kf->keyFrames->image(0),
+        matchedFeatures, newFeatures,valid,err,cv::Size(11,11),3,
+        cv::TermCriteria(cv::TermCriteria::COUNT+cv::TermCriteria::EPS, 30, 0.01),
+        cv::OPTFLOW_LK_GET_MIN_EIGENVALS, 1e-4);
+
+      //again only for debugging
+      //creates image showing matches between the two frames
+      /*
+      cv::Mat outImg(matchedFrame->keyFrames->image(0).rows,
+        matchedFrame->keyFrames->image(0).cols+kf->keyFrames->image(0).cols, CV_8UC1);
+      cv::Mat left(outImg, cv::Rect(0,0,matchedFrame->keyFrames->image(0).cols,matchedFrame->keyFrames->image(0).rows));
+      cv::Mat right(outImg, cv::Rect(matchedFrame->keyFrames->image(0).cols,0,kf->keyFrames->image(0).cols,matchedFrame->keyFrames->image(0).rows));
+      matchedFrame->keyFrames->image(0).copyTo(left);
+      kf->keyFrames->image(0).copyTo(right);
+      for(int i=0; i <matchedFeatures.size(); i++){
+        if(valid[i])
+          line(outImg, matchedFeatures[i],cv::Point2f(newFeatures[i].x+matchedFrame->keyFrames->image(0).cols,newFeatures[i].y),cv::Scalar(255,0,0));
+      }
+      debugImages_.PushNonBlockingDroppingIfFull(outImg, 1);*/
+
+      //get the 3d position of each matched point
+      //and create new feature vector that only uses the points that were matched
+      std::vector<cv::Point3f> matchedPoints3d;
+      for (size_t k = 0; k < matchedFrame->observations.size(); ++k) {
+        if(valid[k]){
+          Eigen::Vector4d matchedPoint4d = matchedFrame->observations[k].landmark_C;
+          cv::Point3f matchedPoint3d(matchedPoint4d[0],matchedPoint4d[1],matchedPoint4d[2]);
+          matchedPoints3d.push_back(matchedPoint3d);
+          newFeaturesFinal.push_back(newFeatures[k]);
+        }
+      }
+
+      //get intrinsics and distortion coeffs from fame
+      Eigen::VectorXd full_intrinsics;
+      kf->keyFrames->geometry(0)->getIntrinsics(full_intrinsics);
+
+      cv::Mat cameraMatrix(3,3,cv::DataType<double>::type);
+      cv::Mat distCoeffs(4,1,cv::DataType<double>::type);
+      cameraMatrix.at<double>(0,0) = full_intrinsics[0];
+      cameraMatrix.at<double>(1,1) = full_intrinsics[1];
+      cameraMatrix.at<double>(0,2) = full_intrinsics[2];
+      cameraMatrix.at<double>(1,2) = full_intrinsics[3];
+
+      distCoeffs.at<double>(0) = full_intrinsics[4];
+      distCoeffs.at<double>(1) = full_intrinsics[5];
+      distCoeffs.at<double>(2) = full_intrinsics[6];
+      distCoeffs.at<double>(3) = full_intrinsics[7];
+
+      //compute the 3d position of the new keyframe relative to the matched frame
+      //we will use ransac to determine which points are inliers
+      cv::Mat rvec(3,1,cv::DataType<double>::type);
+      cv::Mat tvec(3,1,cv::DataType<double>::type);
+
+      std::vector<int> pnpInliers; 
+      solvePnPRansac(matchedPoints3d, newFeaturesFinal, 
+        cameraMatrix, distCoeffs, rvec, tvec, false, 100, 2.0,
+        0.5,pnpInliers);
+
+      //convert to eigen transform
+      cv::Mat R; 
+      cv::Rodrigues(rvec,R);
+      tvec= -R*tvec;
+
+      Eigen::Matrix3d rot;
+      cv::cv2eigen(R,rot);
+
+      Eigen::Vector3d trans;
+      cv::cv2eigen(tvec,trans);
+      Eigen::Affine3d transform_estimate = Eigen::Translation3d(trans)*Eigen::Quaterniond(rot);
+
+      //check if transform is valid, meaning we were able to compute
+      //a transform that enough points agreed upon
+      if(pnpInliers.size()/(float)newFeaturesFinal.size()>0.301){
+        //.301 was experimentally determined to give good results
+        //it is the % required inliers for the loop closure to be good
+
+        //if succesful add a constaint to the pose graph
+        okvis::kinematics::Transformation okvis_estimate(transform_estimate.matrix());
+        Pose3dNode newConstraintNode;
+        newConstraintNode.q = okvis_estimate.q();
+        newConstraintNode.p = okvis_estimate.r();
+        Constraint3dNode newConstraint;
+        newConstraint.t_be = newConstraintNode;
+        newConstraint.id_begin = qret[0].Id;
+        newConstraint.id_end = poses_.size();
+        newConstraint.information = Eigen::Matrix<double,6,6>::Identity()/4;
+
+        constraints_.push_back(newConstraint);
+
+        //perform optimization of pose graph. 
+        ::ceres::Problem problem;
+        BuildOptimizationProblem(&problem);
+        SolveOptimizationProblem(&problem);
+        OutputPoses("poses.txt");
+
+        //update current position and position of new keyframe. 
+        currentKeyframeT_WSo= okvis::kinematics::Transformation(nodes_[poses_.size()].p,nodes_[poses_.size()].q);
+        //kf->T_WS = currentKeyframeT_WSo;
+
+        //std::cout << "LOOP CLOSURE: " << pnpInliers.size()/(float)newFeaturesFinal.size() << "%% inliers" << std::endl;
+        //posesSinceLastLoop_=0;
+      }
+    }
+
+    //add keyframe to graph
+    lastEntry_ = kf->id = db_->add(kf->bowVec);
+    poses_.push_back(kf);
+    posesSinceLastLoop_++;
   }
 
 
